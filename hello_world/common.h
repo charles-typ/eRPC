e3a11cf8f9699704f799f1746b757f45831aee1e
Yupeng Tang <yupeng.tang@yale.edu>
2023-03-18 21:49:53 +0000

    Typo

--- hello_world/common.h
+++ hello_world/common.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "rpc.h"
 
-static const std::string kServerHostname = "10.10.10.213";
-static const std::string kClientHostname = "10.10.10.221";
+static const std::string kServerHostname = "10.10.10.221";
+static const std::string kClientHostname = "10.10.10.213";
 
 static constexpr uint16_t kUDPPort = 31850;
 static constexpr uint8_t kReqType = 2;
