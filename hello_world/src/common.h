acc79c7925eec0196bdc4fc05109cceee00223ee
Yupeng Tang <yupeng.tang@yale.edu>
2023-03-19 01:55:25 +0000

    User control IP address instead of NIC interface address

--- hello_world/src/common.h
+++ hello_world/src/common.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "rpc.h"
 
-static const std::string kServerHostname = "10.10.10.221";
-static const std::string kClientHostname = "10.10.10.213";
+static const std::string kServerHostname = "192.168.122.43";
+static const std::string kClientHostname = "192.168.122.42";
 
 static constexpr uint16_t kUDPPort = 31850;
 static constexpr uint8_t kReqType = 2;
