556d555601b9aa09b1e4348d3cf55795f05a226e
Yupeng Tang <yupeng.tang@yale.edu>
2023-03-29 02:41:21 +0000

    Cleanup

--- hello_world/src/common.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "rpc.h"
-
-static const std::string kServerHostname = "192.168.122.43";
-static const std::string kClientHostname = "192.168.122.42";
-
-static constexpr uint16_t kUDPPort = 31850;
-static constexpr uint8_t kReqType = 2;
-static constexpr size_t kMsgSize = 16;
