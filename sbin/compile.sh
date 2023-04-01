f007914bd2e58ad1e6944a4cfba49594f0e9f0d7
Yupeng Tang <yupeng.tang@yale.edu>
2023-04-01 11:20:27 +0000

    Clean up

--- sbin/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p ../build
-cd ../build
-
-# https://stackoverflow.com/questions/17275348/how-to-specify-new-gcc-path-for-cmake
-export CC=/opt/rh/devtoolset-8/root/usr/bin/gcc && export CXX=/opt/rh/devtoolset-8/root/usr/bin/g++ && cmake ..
-
-#make -j$(nproc)
