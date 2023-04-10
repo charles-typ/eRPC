1a4e18362bea04cbb8b2f18c9614a5b6f43d7b5e
Yupeng Tang <yupeng.tang@yale.edu>
2023-04-10 20:41:34 +0000

    Update

--- linked_list_test.cpp
+++ linked_list_test.cpp
@@ -1,28 +1,51 @@
+#include <chrono>
 #include <iostream>
-
 
 using namespace std;
 
 struct node {
-	uint64_t key;
-	uint64_t value;
-	void* next;
-	char padding[256-8-8-8];
+  uint64_t key;
+  uint64_t value;
+  struct node* next;
+  char padding[4096 - 8 - 8 - 8];
 };
 
 int main() {
-	struct node* start;
-	start = new node;
-	struct node* cur;
-	struct node* prev = start;
-	for(i = 0; i < 100; i++) {
-		cur = new node;
-		prev->next = cur;
-		prev = cur;
-	}
-	total_result;
+  int num_lists = 500000;
+  int list_length = 100;
+  struct node* start[num_lists];
+  struct node* cur[num_lists];
+  struct node* prev[num_lists];
+  for (int j = 0; j < num_lists; j++) {
+	cout << j << endl;
+    start[j] = new node;
+    prev[j] = start[j];
+    for (int i = 0; i < 100; i++) {
+      cur[j] = new node;
+      prev[j]->next = cur[j];
+      prev[j] = cur[j];
+    }
+  }
+  for (int j = 0; j < num_lists; j++) {
+    uint64_t total_result = 0;
+    uint64_t total_time = 0;
+    struct node* search = start[j];
+    int repeat = 10;
+    for (int i = 0; i < repeat; i++) {
+      auto start_time = std::chrono::high_resolution_clock::now();
+      while (search->next != NULL) {
+        total_result += search->value;
+        search = search->next;
+      }
+      auto end_time = std::chrono::high_resolution_clock::now();
+      total_time += static_cast<uint64_t>(
+          std::chrono::duration_cast<std::chrono::nanoseconds>(end_time -
+                                                               start_time)
+              .count());
+      cout << "result is: " << total_result << endl;
+    }
+  	cout << "Traverse time for 100 nodes: " << total_time / repeat << endl;
+  }
 
-
-
-	return 0;
-} 
+  return 0;
+}
