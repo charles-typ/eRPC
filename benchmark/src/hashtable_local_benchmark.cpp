d1d653c63a482ac0b37c0a6fca866a0161bcb956
Yupeng Tang <yupeng.tang@yale.edu>
2023-04-12 23:43:57 +0000

    Clean up

--- benchmark/src/hashtable_local_benchmark.cpp
+++ benchmark/src/hashtable_local_benchmark.cpp
@@ -1,99 +1,117 @@
 
 #include <iostream>
 #include <string>
+
+#include "benchmark_util.h"
 #include "pc/datastructure/hashtable.h"
 #include "pc/parser/rpc_parse.h"
-#include "benchmark_util.h"
 
 using namespace pc::parser;
 using namespace pc::utils;
 
-struct linked_list_node
-{
-    uint64_t key;
-    uint64_t value;
-    struct linked_list_node *next;
-    char padding[256 - 24];
+struct linked_list_node {
+  uint64_t key;
+  uint64_t value;
+  struct linked_list_node *next;
+  char padding[256 - 24];
 };
 
-int main(int argc, char *argv[])
-{
-    if (argc == 2 && strcmp(argv[1], "local") == 0)
-    {
-        //PinToCore(0);
-        std::cout << "Size of node: " << sizeof(struct linked_list_node) << std::endl;
-        struct linked_list_node start;
-        struct linked_list_node *cur = &start;
-        for (int i = 0; i < 1000000; i++)
-        {
-            auto ptr = new (struct linked_list_node);
-            ptr->next = NULL;
-            cur->next = ptr;
-            cur = ptr;
-        }
-        //auto start_t = get_time();
-        cur = &start;
-        uint64_t count = 0;
-        int count_node = 0;
-        while (cur->next != NULL)
-        {
-            count_node++;
-            count += cur->key;
-            count += cur->value;
-            cur->padding[256 - 24 - 1] = 'a';
-            cur = cur->next;
-        }
-        //auto end_1 = get_time();
-        //std::cout << "Takes " << (end_1 - start_t) << " nanoseconds to traverse a " << count_node << " linkedlist" << std::endl;
+int main(int argc, char *argv[]) {
+  if (argc == 2 && strcmp(argv[1], "local") == 0) {
+    // PinToCore(0);
+    std::cout << "Size of node: " << sizeof(struct linked_list_node)
+              << std::endl;
+    struct linked_list_node start;
+    struct linked_list_node *cur = &start;
+    for (int i = 0; i < 1000000; i++) {
+      auto ptr = new (struct linked_list_node);
+      ptr->next = NULL;
+      cur->next = ptr;
+      cur = ptr;
     }
-    else
-    {
-        pc::datastructure::hashtable::HashTable ht;
-        RpcParse parser;
-        std::ifstream data_stream(std::string("/var/data/ycsbc-key-1-blade"));
-        uint64_t num_keys = 100000000;
-        //auto start = get_time();
-        auto start_time_0 = std::chrono::high_resolution_clock::now();
-        parser.read_all_keys(data_stream, num_keys);
-        auto end_time_1 = std::chrono::high_resolution_clock::now();
-        //auto end_1 = get_time();
-        std::cout << "Takes " << static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>(end_time_1-start_time_0).count())/ 1000 / 1000 / 1000 << " seconds to read keys to memory" << std::endl;
-        for (uint64_t i = 0; i < num_keys; ++i)
-        {
-            std::string value = gen_random(7);
-            ht.insert(std::make_pair(parser.all_keys[i], value.c_str()));
-        }
-        auto end_time_2 = std::chrono::high_resolution_clock::now();
-        std::cout << "Takes " << static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>(end_time_2 - end_time_1).count())/ num_keys << " nanoseconds on average to insert a key" << std::endl;
-        //auto end_2 = get_time();
-        //std::cout << "Takes " << (end_2 - end_1) / 1000 / 1000 / 1000 << " seconds to insert to Hashtable " << std::endl;
-
-        ht.print_statistics();
-
-        std::ifstream query_stream(std::string("/var/data/ycsbc-query-1-blade"));
-        uint64_t num_queries = 10000000;
-        //auto end_3 = get_time();
-        parser.read_all_query(query_stream, num_queries);
-        auto end_time_3 = std::chrono::high_resolution_clock::now();
-        std::cout << "Takes " << static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>(end_time_3 - end_time_2).count())/ 1000 / 1000 / 1000 << " seconds to read all queries" << std::endl;
-        //auto end_4 = get_time();
-        //std::cout << "Takes " << (end_4 - end_3) / 1000 / 1000 / 1000 << " seconds to load queries " << std::endl;
-        //PinToCore(0);
-        uint64_t total_time = 0;
-
-	uint64_t total_value = 0;
-        for (uint64_t i = 0; i < num_queries; i++)
-        {
-            auto start_time = std::chrono::high_resolution_clock::now();
-            auto result = ht.find(parser.all_query[i].key);
-	    total_value += *(static_cast<uint64_t*>(result.get_value()));
-            auto end_time = std::chrono::high_resolution_clock::now();
-            total_time += static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>(end_time-start_time).count());
-        }
-        std::cout << "Takes " <<  total_time / num_queries << " nanoseconds on each query in average " << std::endl;
-	std::cout << "The result is " << total_value << std::endl;
-        //auto end_5 = get_time();
-        //std::cout << "Takes " << (end_5 - end_4) << " nanoseconds to execute queries " << std::endl;
+    // auto start_t = get_time();
+    cur = &start;
+    uint64_t count = 0;
+    int count_node = 0;
+    while (cur->next != NULL) {
+      count_node++;
+      count += cur->key;
+      count += cur->value;
+      cur->padding[256 - 24 - 1] = 'a';
+      cur = cur->next;
     }
-    return 0;
+    // auto end_1 = get_time();
+    // std::cout << "Takes " << (end_1 - start_t) << " nanoseconds to traverse a
+    // " << count_node << " linkedlist" << std::endl;
+  } else {
+    pc::datastructure::hashtable::HashTable ht;
+    RpcParse parser;
+    std::ifstream data_stream(std::string("/var/data/ycsbc-key-1-blade"));
+    uint64_t num_keys = 100000000;
+    // auto start = get_time();
+    auto start_time_0 = std::chrono::high_resolution_clock::now();
+    parser.read_all_keys(data_stream, num_keys);
+    auto end_time_1 = std::chrono::high_resolution_clock::now();
+    // auto end_1 = get_time();
+    std::cout << "Takes "
+              << static_cast<uint64_t>(
+                     std::chrono::duration_cast<std::chrono::nanoseconds>(
+                         end_time_1 - start_time_0)
+                         .count()) /
+                     1000 / 1000 / 1000
+              << " seconds to read keys to memory" << std::endl;
+    for (uint64_t i = 0; i < num_keys; ++i) {
+      std::string value = gen_random(7);
+      ht.insert(std::make_pair(parser.all_keys[i], value.c_str()));
+    }
+    auto end_time_2 = std::chrono::high_resolution_clock::now();
+    std::cout << "Takes "
+              << static_cast<uint64_t>(
+                     std::chrono::duration_cast<std::chrono::nanoseconds>(
+                         end_time_2 - end_time_1)
+                         .count()) /
+                     num_keys
+              << " nanoseconds on average to insert a key" << std::endl;
+    // auto end_2 = get_time();
+    // std::cout << "Takes " << (end_2 - end_1) / 1000 / 1000 / 1000 << "
+    // seconds to insert to Hashtable " << std::endl;
+
+    ht.print_statistics();
+
+    std::ifstream query_stream(std::string("/var/data/ycsbc-query-1-blade"));
+    uint64_t num_queries = 10000000;
+    // auto end_3 = get_time();
+    parser.read_all_query(query_stream, num_queries);
+    auto end_time_3 = std::chrono::high_resolution_clock::now();
+    std::cout << "Takes "
+              << static_cast<uint64_t>(
+                     std::chrono::duration_cast<std::chrono::nanoseconds>(
+                         end_time_3 - end_time_2)
+                         .count()) /
+                     1000 / 1000 / 1000
+              << " seconds to read all queries" << std::endl;
+    // auto end_4 = get_time();
+    // std::cout << "Takes " << (end_4 - end_3) / 1000 / 1000 / 1000 << "
+    // seconds to load queries " << std::endl; PinToCore(0);
+    uint64_t total_time = 0;
+
+    uint64_t total_value = 0;
+    for (uint64_t i = 0; i < num_queries; i++) {
+      auto start_time = std::chrono::high_resolution_clock::now();
+      auto result = ht.find(parser.all_query[i].key);
+      total_value += *(static_cast<uint64_t *>(result.get_value()));
+      auto end_time = std::chrono::high_resolution_clock::now();
+      total_time += static_cast<uint64_t>(
+          std::chrono::duration_cast<std::chrono::nanoseconds>(end_time -
+                                                               start_time)
+              .count());
+    }
+    std::cout << "Takes " << total_time / num_queries
+              << " nanoseconds on each query in average " << std::endl;
+    std::cout << "The result is " << total_value << std::endl;
+    // auto end_5 = get_time();
+    // std::cout << "Takes " << (end_5 - end_4) << " nanoseconds to execute
+    // queries " << std::endl;
+  }
+  return 0;
 }
--- linked_list_test.cpp
+++ linked_list_test.cpp
@@ -1,7 +1,7 @@
 #include <chrono>
 #include <iostream>
-#include <vector>
 #include <thread>
+#include <vector>
 #define num_lists 500000
 #define list_length 200
 #define num_threads 1
@@ -21,35 +21,35 @@ vector<vector<uint64_t>> result;
 vector<vector<uint64_t>> total_result;
 
 void func(int thread_id) {
-    int repeat = 20;
-	for (int i = 0; i < repeat; i++) {
-  for (int j = 0; j < num_lists; j++) {
-    //uint64_t total_result = 0;
-    uint64_t total_time = 0;
+  int repeat = 20;
+  for (int i = 0; i < repeat; i++) {
+    for (int j = 0; j < num_lists; j++) {
+      // uint64_t total_result = 0;
+      uint64_t total_time = 0;
       struct node* search = start[j];
       auto start_time = std::chrono::high_resolution_clock::now();
-	  int count = 0;
+      int count = 0;
       while (search->next != NULL && search->key != 100) {
-		count++;
+        count++;
         total_result[thread_id][j] += search->value;
         search = search->next;
       }
-	  //std::cout << "Traverse length = " << count << std::endl;
+      // std::cout << "Traverse length = " << count << std::endl;
       auto end_time = std::chrono::high_resolution_clock::now();
       total_time += static_cast<uint64_t>(
           std::chrono::duration_cast<std::chrono::nanoseconds>(end_time -
                                                                start_time)
               .count());
-      //cout << "result is: " << total_result << endl;
-    result[thread_id][j] = total_time;
-  }
+      // cout << "result is: " << total_result << endl;
+      result[thread_id][j] = total_time;
+    }
   }
 }
 
 int main() {
-	start.reserve(num_lists);
-	cur.reserve(num_lists);
-	prev_node.reserve(num_lists);
+  start.reserve(num_lists);
+  cur.reserve(num_lists);
+  prev_node.reserve(num_lists);
   int j = 0;
   for (; j < num_lists; j++) {
     start[j] = new node;
@@ -65,20 +65,21 @@ int main() {
   vector<thread> thread_vec;
   result.reserve(num_threads);
   total_result.reserve(num_threads);
-  for(int i = 0; i < num_threads; i++) {
-	result[i].reserve(num_lists);
-	total_result[i].reserve(num_lists);
-	std::thread t(func, i);
-	thread_vec.push_back(std::move(t));
+  for (int i = 0; i < num_threads; i++) {
+    result[i].reserve(num_lists);
+    total_result[i].reserve(num_lists);
+    std::thread t(func, i);
+    thread_vec.push_back(std::move(t));
   }
-  for(auto &t: thread_vec) {
-	t.join();
+  for (auto& t : thread_vec) {
+    t.join();
   }
-  for(int i = 0; i < num_threads; i++) {
-  	for (int j = 0; j < num_lists; j++) {
-		cout << "Thread[" << i << "] List[" << j << "]: " << result[i][j] << " nanoseconds" << std::endl;
-		cout << "result is: " << total_result[i][j] << endl;
-	}
+  for (int i = 0; i < num_threads; i++) {
+    for (int j = 0; j < num_lists; j++) {
+      cout << "Thread[" << i << "] List[" << j << "]: " << result[i][j]
+           << " nanoseconds" << std::endl;
+      cout << "result is: " << total_result[i][j] << endl;
+    }
   }
 
   return 0;
